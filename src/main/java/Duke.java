7a80cc71d178867b2f39b8e369e4a1d41161cb59
marcus <101387980+jamestorivor@users.noreply.github.com>
2026-09-04 11:37:09 +0800

    Add part1,2,3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
